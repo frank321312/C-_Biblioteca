9ae9ab2389527a9551e026de9688eb6d1969f9d5
frank321312 <hectorsacaca1123@gmail.com>
2024-03-27 11:36:32 -0300

    Commit final

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Libreria;
 
-Cuenta cuenta = new Cuenta(60000);
-Cliente cliente = new Cliente("Pepito", "El mago", 10100, cuenta);
+Cuenta cuenta = new Cuenta(3000);
+Cliente cliente = new Cliente("Pepito", "El mago", 40100, cuenta);
 
 // System.Console.WriteLine(cliente.DevolverSaldo());
-cliente.DebitarEfectivo(30000);
+// cliente.DebitarEfectivo(40000);
+cuenta.Debitar(cliente, 40000);
+System.Console.WriteLine(cliente.DevolverSaldo());
+System.Console.WriteLine(cliente.cuenta.DevolverSaldo());
+System.Console.WriteLine(cliente._estado);
+cuenta.Debitar(cliente, 1000);
 System.Console.WriteLine(cliente.DevolverSaldo());
 System.Console.WriteLine(cliente.cuenta.DevolverSaldo());
 System.Console.WriteLine(cliente._estado);
